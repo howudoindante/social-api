f7bfc79a3729275cdfa8f4cb2dd19b37d0175812
Marcus Blanco <xenxenoxbeats@gmail.com>
2022-07-14 23:03:34 +0400

    Init project and prisma models

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { join } from 'path';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+    AuthModule,
+  ],
+})
+export class AppModule {}
